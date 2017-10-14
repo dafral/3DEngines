6c6dc37a03d77a290e5831daeac32f7eb6d91610
Marc de Pedro <marcdp2012@gmail.com>
2017-10-15 00:44:06 +0200

    Texture on geometry works

--- 3D Engine/ModuleGeometry.cpp
+++ 3D Engine/ModuleGeometry.cpp
@@ -107,7 +107,6 @@ void ModuleGeometry::LoadGeometry(const char* full_path)
 				glBindBuffer(GL_ARRAY_BUFFER, (GLuint)data.id_uvs);
 				glBufferData(GL_ARRAY_BUFFER, sizeof(uint) * data.num_uvs * 3, data.texture_coords, GL_STATIC_DRAW);
 			}		
-
 			meshes.push_back(data);
 		}
 		aiReleaseImport(scene);
@@ -129,40 +128,39 @@ void ModuleGeometry::LoadTexture(const char* full_path)
 	{
 		ILinfo ImageInfo;
 		iluGetImageInfo(&ImageInfo);
+
 		//Flip the image if it is upside-down
-		if (ImageInfo.Origin == IL_ORIGIN_UPPER_LEFT)
-		{
+		if (ImageInfo.Origin == IL_ORIGIN_UPPER_LEFT) {
 			iluFlipImage();
 		}
+
 		if (!ilConvertImage(IL_RGBA, IL_UNSIGNED_BYTE))
 		{
 			error = ilGetError();
-			//LOG("Texture Image conversion Failed: %d %s", error, iluErrorString(error));
+			CONSOLELOG("Texture conversion failed: %d %s", error, iluErrorString(error));
 		}
-		else {
+		else 
+		{
+			// Enabling texture
+			glEnable(GL_TEXTURE_2D);
 
 			glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
 			glGenTextures(1, &tex.id_texture);
 			glBindTexture(GL_TEXTURE_2D, tex.id_texture);
 
-			//Clamping Method
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-			// Interpolation Method
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
 			//Texture Specifications
-			glTexImage2D(GL_TEXTURE_2D, 0, ilGetInteger(IL_IMAGE_FORMAT), ilGetInteger(IL_IMAGE_WIDTH), ilGetInteger(IL_IMAGE_HEIGHT), 0, ilGetInteger(IL_IMAGE_FORMAT), GL_UNSIGNED_BYTE, ilGetData());
-
-			glBindTexture(GL_TEXTURE_2D, 0);
+			glTexImage2D(GL_TEXTURE_2D, 0, ilGetInteger(IL_IMAGE_FORMAT), ilGetInteger(IL_IMAGE_WIDTH), ilGetInteger(IL_IMAGE_HEIGHT), 0, ilGetInteger(IL_IMAGE_FORMAT), GL_UNSIGNED_BYTE, ilGetData());	
 		}
 	}
 	else {
 		error = ilGetError();
 	}
-
 
 	ilDeleteImages(1, &imageID);
 }
@@ -176,16 +174,13 @@ void ModuleGeometry::Draw()
 		glVertexPointer(3, GL_FLOAT, 0, NULL);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, meshes[i].id_indices);
 
+		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+		glBindBuffer(GL_ARRAY_BUFFER, meshes[i].id_uvs);
+		glTexCoordPointer(3, GL_FLOAT, 0, NULL);
+
 		glBindTexture(GL_TEXTURE_2D, (GLuint)tex.id_texture);
 		glDrawElements(GL_TRIANGLES, meshes[i].num_indices, GL_UNSIGNED_INT, NULL);
 		
-		if (meshes[i].num_uvs != 0)
-		{
-			glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-			glBindBuffer(GL_ARRAY_BUFFER, meshes[i].id_uvs);
-			glTexCoordPointer(3, GL_FLOAT, 0, NULL);
-		}
-
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
