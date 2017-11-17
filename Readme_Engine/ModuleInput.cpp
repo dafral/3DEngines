858c420a9299d7acb409939f86cc2ec41d85e47b
dafral <dafral1996@gmail.com>
2017-11-17 18:29:20 +0100

    DDS now load correctly

--- Readme_Engine/ModuleInput.cpp
+++ Readme_Engine/ModuleInput.cpp
@@ -143,11 +143,10 @@ update_status ModuleInput::PreUpdate(float dt)
 				{
 					if (App->imgui->hierarchy->go_selected != nullptr)
 					{
-						string tex_path;
-						if (App->material_imp->Import("prueba", dropped_filedir, tex_path)) {
-							App->geometry->LoadMaterial(dropped_filedir, App->imgui->hierarchy->go_selected);
-							App->imgui->properties->SetTextureName(dropped_filedir);
-						}
+						App->material_imp->Import(str.c_str());
+						App->geometry->LoadMaterial(dropped_filedir, App->imgui->hierarchy->go_selected);
+						App->imgui->properties->SetTextureName(dropped_filedir);
+						
 					}
 					else
 					{
